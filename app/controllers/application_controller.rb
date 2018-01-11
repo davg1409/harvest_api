9e5353749566809eb56a3d45be9e4e3a818691dd
Bhavesh <bhaveshkshatriya.ror@gmail.com>
2018-01-11 11:56:20 +0530

    handle record not found error for json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  private
+    def render_404 exception = nil
+      logger.info "Rendering 404: #{exception.message}" if exception
+
+      render json: {
+        error: "Record Not Found!"
+      }, status: 404 and return
+    end
 end
