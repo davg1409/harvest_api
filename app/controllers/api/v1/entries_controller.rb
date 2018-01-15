6ebdc09d0b2a8bba2c5909ce2ceb690943716654
Bhavesh <bhaveshkshatriya.ror@gmail.com>
2018-01-15 17:26:58 +0530

    Remove records if they are not included in the request.

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::EntriesController < ApplicationController
   end
 
   def update
-    if @entry.update entry_params
+    if @entry.update entry_update_params
       render :show
     else
       render json: { errors: @entry.errors.full_messages }, status: 422
@@ -33,5 +33,20 @@ class Api::V1::EntriesController < ApplicationController
     def entry_params
       params[:entry][:entry_items_attributes] = params[:entry][:items] if params[:entry].present?
       params.require(:entry).permit(:date, :name, :note, :amount, :entry_type, tag_ids: [],   entry_items_attributes: [:amount, :dc, :chart_account_id, tag_ids: []])
+    end
+
+    def entry_update_params
+      params[:entry] ||= {}
+      item_ids = @entry.entry_items.map(&:id)
+      params[:entry][:items] ||= []
+      param_item_ids = params[:entry][:items].map { |item| item[:id] }.compact
+
+      item_ids.each do |item_id|
+        if param_item_ids.exclude?(item_id.to_s)
+          params[:entry][:items] << {id: item_id, _destroy: true}
+        end
+      end
+      params[:entry][:entry_items_attributes] = params[:entry][:items] if params[:entry].present?
+      params.require(:entry).permit(:date, :name, :note, :amount, :entry_type, tag_ids: [],   entry_items_attributes: [:id, :amount, :dc, :chart_account_id, :_destroy, tag_ids: []])
     end
 end 
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,7 +8,7 @@ class Entry < ApplicationRecord
 
   enum entry_type: [:check, :deposit, :expense, :transfer]
 
-  accepts_nested_attributes_for :entry_items
+  accepts_nested_attributes_for :entry_items, allow_destroy: true
 
   validates_presence_of :account, :entry_type, :name, :date, :amount
   validates_uniqueness_of :name, scope: :account_id
