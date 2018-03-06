4e5d818ac960ac0bdf455fbf2f0ab59243d21372
dev <davg1409@outlook.com>
2018-03-06 18:21:44 +0800

    changed email optional

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,3 @@
 class Customer < ApplicationRecord
     validates_presence_of :name
-    validates_presence_of :email
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,3 @@
 class Vendor < ApplicationRecord
     validates_presence_of :name
-    validates_presence_of :email
 end
